cfbaf61763b0620ab487a4d92d63af12e0e19a15
SuKyoung Shin <sukyoung.dev@gmail.com>
2023-01-17 22:44:10 +0900

    chore: 필요모듈 설치 및 env 추가

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Axios from "axios";
 
 export default function App({ Component, pageProps }: AppProps) {
+  Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
+
   return <Component {...pageProps} />;
 }
